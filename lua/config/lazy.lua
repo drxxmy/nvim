c87b7d91a59c736c3413ab74bfb2c43f9ad5c367
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-04-19 12:38:09 +0500

    (lazy) slight changes in comments

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,17 +17,16 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+-- Setup keymaps and options
 require('config.keymaps')
 require('config.options')
 
 -- Setup lazy.nvim
 require('lazy').setup({
   spec = {
-    -- import your plugins
     { import = 'plugins' },
   },
   -- automatically check for plugin updates
